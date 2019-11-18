b1b6dfdceb544af70b59a5fc5d78957ef593b08f
Lowpower <szq123456123@gmail.com>
2019-11-18 08:37:17 +0800

    add 2-1

--- happyAlgorithm/2-1.cpp
+++ happyAlgorithm/2-1.cpp
@@ -6,5 +6,23 @@ using namespace std;
 double w[N];
 
 int main() {
+	double c;
+	int n;
+	cin >> c>> n;
+	for(int i = 0; i  < n;i++) {
+		cin>>w[i];
+	}
+	sort(w, w+n);
+	double temp = 0.0;
+	int ans = 0;
+	for(int i = 0; i < n; i++) {
+		temp +=w[i];
+		if(temp <= c) {
+			ans++;
+		}else {
+			break;
+		}
+	}
+	cout << ans << endl;
 	return 0;
 }
