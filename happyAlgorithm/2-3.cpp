21b5fbc2de595ee5b0df39bdbc6cca115149d299
Lowpower <szq123456123@gmail.com>
2019-11-19 08:36:34 +0800

    add 2-3

---
+++ happyAlgorithm/2-3.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct Meet {
+	int beg;
+	int end;
+	int num;
+}meet[1000];
+
+class SetMeet {
+	public:
+		void init();
+		void solve();
+	private:
+		int n,ans;
+};
+
+int main() {
+	return 0;
+}
