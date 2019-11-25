40bbddef48ddd5f5cae2dffabd8e9c443231436c
Lowpower <szq123456123@gmail.com>
2019-11-25 08:25:35 +0800

    add 2-3

--- happyAlgorithm/2-3.cpp
+++ happyAlgorithm/2-3.cpp
@@ -18,6 +18,29 @@ class SetMeet {
 		int n,ans;
 };
 
+void SetMeet::init() {
+	int s,e;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s >> e;
+		meet[i].beg = s;
+		meet[i].end = e;
+		meet[i].num = i+1;
+	}
+}
+
+void SetMeet::solve() {
+	sort(meet, meet+n, [](Meet x, Meet y) {
+		if(x.end == y.end) {
+			return x.beg > y.beg;
+		}
+		return x.end < y.end;
+	});
+}
+
 int main() {
+	SeetMeet sm;
+	sm.init();
+	sm.solve();
 	return 0;
 }
