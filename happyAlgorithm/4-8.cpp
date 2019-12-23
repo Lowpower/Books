c562a62f9c52e0b0048d55f647df142063cc52d7
Lowpower <szq123456123@gmail.com>
2019-12-23 08:35:21 +0800

    update 4-8

--- happyAlgorithm/4-8.cpp
+++ happyAlgorithm/4-8.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-using namesapce std;
+const int N = 205;                                                                                                                                                                                                             int  
+int Max[N][N];
 
+void straight(vector<int>& a, int n) {
+    for(int i = 0; i < n; i++) {
+        
+    }
+}
 int main() {
     int n = 0;
+    cin >> n;
+    int inMunber;
+    vector<int> a;
+    for (int i = 0; i < n; i++) {
+        cin >> inMunber;
+        a.push_back(inMunber);
+    }
+    straight(a, n);
     return 0;
 }
