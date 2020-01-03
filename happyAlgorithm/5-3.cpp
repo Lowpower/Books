390236055e4a529a79921b281362f249971bdaeb
Lowpower <szq123456123@gmail.com>
2020-01-03 08:27:45 +0800

    update 5-3

--- happyAlgorithm/5-3.cpp
+++ happyAlgorithm/5-3.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
+const int MX = 50;
+int map[MX][MX];
+int x[MX];
+int ssum = 0;
 
-int main() {
+int main()
+{
+    int n, m;
+    cin >> n;
+    cin >> m;
+    cout << "input:" << endl;
+    int u, v, edge;
+    cin >> edge;
+    memset(map, 0, sizeof(map));
+    for (int i = 1; i <= edge; i++)
+    {
+        cin >> u >> v;
+        map[u][v] = map[v][u] = 1;
+    }
+    int sum = 0;
+    auto Backtrack = [&](int t) {
+        if (t > n)
+        {
+            sum++;
+            cout << "sum:" << sum << "  :";
+            for (int i = 1; i <= m; i++)
+            {
+                cout << x[i] << "    ";
+            }
+            cout << endl;
+        } else {
+
+        }
+    };
     return 0;
 }
