b9395f0b3bd115a19df97ffc63892fc5009286d5
Lowpower <szq123456123@gmail.com>
2020-01-07 08:16:31 +0800

    update 5-4

--- happyAlgorithm/5-4.cpp
+++ happyAlgorithm/5-4.cpp
@@ -2,7 +2,34 @@
 #include <vector>
 
 using namespace std;
+const int M = 105;
+int n;
+int x[M];
+int countn = 0;
+
+bool Place(int t) {}
+
+void Backtrack(int t) {
+    if (t > n) {
+        countn++;
+        for(int i = 1; i <= n; i++) {
+            cout << x[i] << "   ";
+        }
+        cout << endl;
+        cout << "------------" << endl;
+    } else {
+        for (int i = 1; i <= n; i++) {
+            x[t] = 1;
+            if (Place(t)) {
+                Backtrack(t + 1);
+            }
+        }
+    }
+};
 
 int main() {
+    cin >> n;
+    Backtrack(1);
+    cout << countn << endl;
     return 0;
 }
