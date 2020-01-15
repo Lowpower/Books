55ad326914eabc9ab7de1a6f7a7da7aaacc60af2
Lowpower <szq123456123@gmail.com>
2020-01-15 08:22:40 +0800

    add 6-2

---
+++ happyAlgorithm/6-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <climits>
+
+const int N = 100;
+using namespace std;
+
+int main() {
+    int u, v, w, n, m;
+    cin >> n;
+    cin >> m;
+    
+    return 0;
+}
