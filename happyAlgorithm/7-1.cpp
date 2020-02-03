78238f767a0a489c56505c410d44fcbf59aa345d
Lowpower <szq123456123@gmail.com>
2020-02-03 17:23:34 +0800

    update 7-1

--- happyAlgorithm/7-1.cpp
+++ happyAlgorithm/7-1.cpp
@@ -3,5 +3,28 @@
 using namespace std;
 
 int main () {
+    cout << "" << endl;
+    cin >> m;
+    for (int i = 1; i <= m; i++) {
+        cin >> FJL[i];
+    }
+    cout << "" << endl;
+    cin >> n;
+    for(int i = 0; i <= n; n++) {
+        cin >> JL[i];
+    }
+    float max1;
+    float max2;
+    int e = -1;
+    int k = -1;
+    float min;
+    while(true) {
+        max1 = 0;
+        max2 = 0;
+        min = 1e8;
+        for (int j = 1; j <= m; j++) {
+            
+        }
+    }
     return 0;
 }
