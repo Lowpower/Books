0764d984fcea9bb8ece6ea8ec67bd8b593877858
Lowpower <szq123456123@gmail.com>
2020-02-06 23:30:51 +0800

    add 4-8-1

---
+++ happyAlgorithm/4-8-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+const int M=1000+5;
+double p[M], q[M];
+
+int main () {
+    int n;
+    cin >> n;
+    for(int i = 0; i <=n; i++) {
+        cin >> p[i];
+    }
+    return 0;
+}
