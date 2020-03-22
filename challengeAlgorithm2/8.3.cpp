4f832603ef6830ccffc640ca886d3c15a480121f
Lowpower <szq123456123@gmail.com>
2020-03-22 21:56:26 +0800

    update

---
+++ challengeAlgorithm2/8.3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 500;
+struct Node {
+    int p, l, r;
+};
+
+struct Node T[MAX];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        T[i] = nil;
+    }
+    return 0;
+}
