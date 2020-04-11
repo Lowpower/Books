707da67c75bbdf4d1efc0f17f12168baddd168b2
Lowpower <szq123456123@gmail.com>
2020-04-12 06:21:10 +0800

    update

--- challengeAlgorithm2/8.2.cpp
+++ challengeAlgorithm2/8.2.cpp
@@ -8,9 +8,28 @@ struct Node {
     int parent, left, right;
 };
 Node T[MAX];
+int D[MAX];
+int H[MAX];
 
-void sserDepth() {
-    
+void setDepth(int u, int d) {
+    if(u == NIL) {
+        return;
+    }
+    D[u] = d;
+    setDepth(T[u].left, d+1);
+    setDepth(T[u].right, d+1);
+}
+
+void setHeight(int u) {
+    int h1 = 0;
+    int h2 = 0;
+    if(T[u].left != NIL) {
+        h1 = setHeight(T[u].left) + 1;
+    }
+    if(T[u].right != NIL) {
+        h2 = setHeight(T[u].right) + 1;
+    }
+    return H[u] = max(h1, h2);
 }
 
 int main() {
