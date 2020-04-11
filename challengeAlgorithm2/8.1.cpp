3a0a2ebac01e600298c495ce3253f947ac464001
Lowpower <szq123456123@gmail.com>
2020-04-11 18:48:46 +0800

    update

--- challengeAlgorithm2/8.1.cpp
+++ challengeAlgorithm2/8.1.cpp
@@ -4,17 +4,41 @@ using namespace std;
 constexpr int MAX = 10000;
 constexpr int NIL = -1;
 
-typedef struct Node {
+ struct Node {
     int p, l,  r;
 };
 Node T[MAX];
 int n, D[MAX];
 
-void print() {
-
+void print(int u) {
+    cout << "node " << u << ": ";
+    cout << "parent = " << T[u].p << ", ";
+    cout << "depth = " << D[u] << ", ";
+    if (T[u].p == NIL) {
+        cout << "root, ";
+    } else if (T[u].l == NIL) {
+        cout << "leaf, ";
+    } else  {
+        cout << "internal node, ";
+    }
+    cout << "[";
+    for (int i = 0, c = T[u].l; c !=NIL; i++, c = T[c].r) {
+        if(i) {
+            cout << ", ";
+        }
+        cout << c;
+    }
+    cout << "]" << endl;
 }
 
 int rec(int u, int p) {
+    D[u] = p;
+    if(T[u].r != NIL) {
+        rec(T[u].r, p);
+    }
+    if(T[u].l != NIL) {
+        rec(T[u].l, p +1);
+    }
     return 0;
 }
 
@@ -24,7 +48,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         T[i].p = T[i].l = T[i].r = NIL;
     }
-    int v, d, c;
+    int v, d, c, l;
     for (int i = 0; i < n; i++) {
         cin >> v >> d;
         for (int j = 0; j < d; j++) {
@@ -32,18 +56,19 @@ int main() {
             if(j == 0) {
                 T[v].l = c;
             } else {
-                T[v].r = c;
+                T[l].r = c;
             }
             l = c;
             T[c].p = v;
         }
     }
+    int root = 0;
     for (int i = 0; i < n; i++) {
         if(T[i].p == NIL) {
-            r = i;
+            root = i;
         }
     }
-    rec(r, 0);
+    rec(root, 0);
     for (int i = 0; i < n; i++) {
         print(i);
     }
