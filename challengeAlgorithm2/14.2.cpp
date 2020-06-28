367d62fc704f9f67410308bfd4ffdabeba1e88b6
Lowpower <szq123456123@gmail.com>
2020-06-29 07:48:36 +0800

    add 14.2

---
+++ challengeAlgorithm2/14.2.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Node {
+    int location;
+    int p, l, r;
+    Node(){}
+};
+int N;
+static const int MAX = 1000000;
+static const int NIL = -1;
+Node P[MAX];
+
+int main() {
+    int x, y;
+    scanf("%d", N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &x, &y);
+
+    }
+    return 0;
+}
