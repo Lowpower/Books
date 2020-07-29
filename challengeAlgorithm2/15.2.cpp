67dcca5153620799bd2988376efa24cccefea30f
Lowpower <szq123456123@gmail.com>
2020-07-30 07:57:37 +0800

    update

--- challengeAlgorithm2/15.2.cpp
+++ challengeAlgorithm2/15.2.cpp
@@ -5,7 +5,24 @@
 #include <list>
 
 using namespace std;
+static const int MAX = 100000;
+static const int INFTY = (1 << 29);
+
+vector<int> G[MAX];
+list<int> out;
+bool V[MAX];
+int N;
+int indeg[MAX];
 
 int main() {
+    int s, t, M;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        V[i] = false;
+    }
+    for (int i = 0; i < M; i++) {
+        cin >> s >> t;
+        G[s].push_back(t);
+    }
     return 0;
 }
