92a9e3a59a5b0f8e399c87cfa12709a227859bdf
Lowpower <szq123456123@gmail.com>
2020-10-14 21:46:36 +0800

    add queue

---
+++ c++11STL/contadapt/queue2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include "Queue.hpp"
+
+using namespace std;
+
+int main() {
+    try {
+        Queue<string> q;
+        q.push("These ");
+        q.push("are ");
+        q.push("more than ");
+        cout << q.pop();
+        cout << q.pop();
+        q.push("four ");
+        q.push("words!");
+  
+        q.pop();
+        cout << q.pop();
+        cout << q.pop() << endl;
+        cout << "number of elements in the queue: " << q.size() << endl;
+        cout << q.pop() << endl;
+    } catch(const exception& e) {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
