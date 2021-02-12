71227092e07d940c295cc275ed783c3b389441fd
Lowpower <szq123456123@gmail.com>
2021-02-12 16:55:52 +0800

    update 2

--- c++17STL/2.2.cpp
+++ c++17STL/2.2.cpp
@@ -2,8 +2,33 @@
 #include <vector>
 #include <algorithm>
 
+template<typename T>
+void quick_remove_at(std::vector<T> &v, std::size_t idx) {
+  if(idx < v.size()) {
+    v[idx] = std::move(v.back());
+    v.pop_back();
+  }
+}
+
+template<typename T>
+void quick_remove_at(std::vector<T> &v, typename std::vector<T>::iterator it) {
+  if(it != std::end(v)) {
+    *it = std::move(v.back());
+    v.pop_back();
+  }  
+}
+
 int main() {
   std::vector<int> v{123, 456, 789, 100, 200};
-  quic_remove_at(v, 2);
+  quick_remove_at(v, 2);
+  for (int i: v) {
+    std::cout << i << ", ";
+  }
+  std::cout << std::endl;
+  quick_remove_at(v, std::find(std::begin(v), std::end(v), 123));
+  for (int i: v) {
+    std::cout << i << ", ";
+  }
+  std::cout << std::endl;
   return 0;  
 }
