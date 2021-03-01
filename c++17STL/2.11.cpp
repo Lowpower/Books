dc5b773ba423c12086c3a3a3f014afdddcd3f64c
Lowpower <szq123456123@gmail.com>
2021-03-02 07:47:27 +0800

    add 2.11

---
+++ c++17STL/2.11.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+string filter_punctuation(const string&s) {
+  const char *forbidden(".,:; ");
+  const auto idx_start(s.find_first_not_of(forbidden));
+  const auto idx_end(s.find_last_not_of(forbidden));
+  return s.substr(idx_start, idx_end - idx_start+1);
+}
+
+int main() {
+  map<string, size_t> words;
+  int max_word_len{0};
+  string s;
+  while(cin>>s) {
+    auto filtered(filter_punctuation(s));
+    max_word_len = max<int>(max_word_len, filtered.length());
+    ++words[filtered];
+  }
+  vector<pair<string, size_t>> word_count;
+  word_count.reserve(words.size());
+  move(begin(words), end(words), back_inserter(word_count));
+  sort(begin(word_count), end(word_count),
+  [](const auto &a, const auto &b){
+    return a.second > b.second;
+  });
+  cout << "#" << setw(max_word_len) << "WORD" << "#COUNT\n";
+  for (const auto &[word, count] : word_count) {
+    cout << setw(max_word_len+2) << word << "#"
+    <<count << "\n";
+  }
+  return 0;
+}
